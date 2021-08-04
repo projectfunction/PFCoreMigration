86cec3546c3fc629c6a2cbaf66cc08a8f60c5881
Daryl Cecile <6310278+daryl-cecile@users.noreply.github.com>
2021-08-04 17:21:52 +0100

    Added rewrite for splitbee

--- next.config.js
+++ next.config.js
@@ -37,6 +37,14 @@ module.exports = {
 				source: '/robot.txt',
 				destination: '/api/robot',
 			},
+			{
+				source: "/bee.js",
+				destination: "https://cdn.splitbee.io/sb.js",
+			},
+			{
+			      source: "/_hive/:slug",
+			      destination: "https://hive.splitbee.io/:slug",
+			}
 		]
 	},
 	async headers() {
