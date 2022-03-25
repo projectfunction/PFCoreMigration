6ce3e6d94f42499af36e99804b985fde8c10d33d
Daryl <darylcecile@gmail.com>
2022-03-25 13:17:54 +0000

    updated image

--- components/JoinSection.tsx
+++ components/JoinSection.tsx
@@ -17,7 +17,7 @@ export default function JoinSection({}){
 				signupLink: "https://campus.projectfunction.io/x/enrol/wdb22022ts40",
 				duration: "9 Weeks",
 				location: "Nonsuch Studios, 2 Lower Parliament St, Nottingham, NG1 1EH",
-				startDate: dateParse("29nd mar 2022", "DD MMM YYYY")
+				startDate: dateParse("12 apr 2022", "DD MMM YYYY")
 			},
 			{
 				name: "INTRO TO\nNEXT.JS",
